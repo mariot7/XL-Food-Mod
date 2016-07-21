99834160b94ea236f9635a06eb74335cb93ed4f2
mariot7 <mariot789@gmail.com>
2016-07-21 18:52:22 +0200

    v0.2.0

--- src/main/java/mariot7/xlfoodmod/Reference.java
+++ src/main/java/mariot7/xlfoodmod/Reference.java
@@ -6,6 +6,6 @@ public class Reference {
 	public static final String VERSION = "1.9.4-0.2.0";
 	public static final String CLIENT_PROXY_CLASS = "mariot7.xlfoodmod.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "mariot7.xlfoodmod.proxy.CommonProxy";
-	public static final String updateJSON = "https://github.com/mariot7/XL-Food-Mod/blob/master/update.json";
+	public static final String updateJSON = "https://raw.githubusercontent.com/mariot7/XL-Food-Mod/master/update.json";
 
 }
